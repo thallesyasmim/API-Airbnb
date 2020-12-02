9c85a500eb0dd59896ff5f8f471004bfa8008a0e
Thalles <thallesgabriel1307@gmail.com>
2020-12-02 12:12:19 -0300

    Add Env Import

--- app/Models/Image.js
+++ app/Models/Image.js
@@ -2,7 +2,7 @@
 
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
 const Model = use('Model')
-const Env = use('Env')
+const Env = use('Env') /* Add Import */ 
 
 class Image extends Model {
     static get computed()  {
